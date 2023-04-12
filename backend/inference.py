1f80d0e4b729fdc5d7e0c1c7f5950927d34257c3
mach-12 <89384052+mach-12@users.noreply.github.com>
2023-04-12 11:48:03 +0530

    predict API

--- backend/inference.py
+++ backend/inference.py
@@ -3,5 +3,5 @@ from SpamDetection import SpamDetection
 def calculate_score(text: str):
     spam_model = SpamDetection() 
     score = spam_model.predict(text)
-    response = {"score":score}
-    return response
+
+    return score
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from model import Infer
+# from model import Infer
 
 # Cross Origin Resourse Sharing - When frontend client JS communicates with Backend. They are in diffrent origin. (Protocol, Domain, Port)
 # We need permission in the backend to interact 
@@ -24,8 +24,8 @@ app.add_middleware(
 def read_root():
     return {"Hello":"World"}
 
-@app.get("/api/predict/{text}/", response_model=Infer)
-async def predict_score(text):
+@app.get("/api/predict/{text}/")
+async def predict_score(text) -> str:
     response = calculate_score(text)
 
     if response:
