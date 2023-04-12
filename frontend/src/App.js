bc7140ca226951ca64d8390e9d8b7b148e0c824e
PlasmicZ <aviralkamaljain@gmail.com>
2023-04-12 11:07:46 +0530

    pudhingby salik

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
 import React from "react";
-import "daisyui/dist/daisyui.css";
+import daisyui from "daisyui";
 
 export default function App() {
   return (
     <div className="bg-white rounded-lg shadow-md p-8 max-w-lg mx-auto mt-8">
-      <h1 className="text-3xl font-bold underline text-red-500 mb-4">
-        Compose Email
+      <h1 className="text-3xl font-bold underline text-red-500 mb-4 text-center">
+        Email
       </h1>
       <form className="flex flex-col">
-        <input
-          type="text"
-          className="input mb-4"
-          placeholder="To"
-          // Add additional classes from daisyUI for input styling
-        />
-        <input
-          type="text"
-          className="input mb-4"
-          placeholder="Subject"
-          // Add additional classes from daisyUI for input styling
-        />
         <textarea
           className="textarea mb-4"
           placeholder="Message"
@@ -30,7 +18,7 @@ export default function App() {
           className="btn btn-primary w-full"
           // Add additional classes from daisyUI for button styling
         >
-          Send
+          Evaluate
         </button>
       </form>
     </div>
