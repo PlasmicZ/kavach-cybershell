1e9c85df973ca14a089d0f6b72c60d9dc63c9fc1
mach-12 <89384052+mach-12@users.noreply.github.com>
2023-04-12 12:15:46 +0530

    added endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,58 @@
-import React from "react";
-import daisyui from "daisyui";
+import React, { useState } from "react";
 
-export default function App() {
+const App = () => {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`/api/predict/${text}/`);
+      if (response.ok) {
+        const data = await response.json();
+        setResult(data);
+        setError(null);
+      } else {
+        setError(`Error in fetching score of text: ${text}`);
+        setResult(null);
+      }
+    } catch (error) {
+      setError(`Error in fetching score of text: ${text}`);
+      setResult(null);
+    }
+  };
+
   return (
-    <div className="bg-white rounded-lg shadow-md p-8 max-w-lg mx-auto mt-8">
-      <h1 className="text-3xl font-bold underline text-red-500 mb-4 text-center">
-        Email
-      </h1>
-      <form className="flex flex-col">
-        <textarea
-          className="textarea mb-4"
-          placeholder="Message"
-          // Add additional classes from daisyUI for textarea styling
-        ></textarea>
-        <button
-          type="submit"
-          className="btn btn-primary w-full"
-          // Add additional classes from daisyUI for button styling
-        >
-          Evaluate
-        </button>
-      </form>
+    <div className="bg-gray-200 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-4">Text Prediction App</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="w-full border-gray-300 border p-2 rounded-md mb-4"
+            placeholder="Enter text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md"
+          >
+            Predict
+          </button>
+        </form>
+        {result && (
+          <div className="mt-4">
+            <h2 className="text-xl font-semibold">Prediction:</h2>
+            <p className="mt-2">{result}</p>
+          </div>
+        )}
+        {error && <p className="text-red-500 mt-4">{error}</p>}
+      </div>
     </div>
   );
-}
+};
+
+export default App;
